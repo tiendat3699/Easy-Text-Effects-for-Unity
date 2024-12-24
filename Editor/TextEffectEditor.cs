2960d1daa8f916b948eaa5ed0213777db84be021
Qiaozhi Lei <georgeleiqiaozhi@gmail.com>
2024-12-23 18:20:17 -0800

    editor ns

--- Editor/TextEffectEditor.cs
+++ Editor/TextEffectEditor.cs
@@ -1,114 +1,110 @@
-using EasyTextEffects;
-using UnityEngine;
 using UnityEditor;
+using UnityEngine;
 
-[CustomEditor(typeof(TextEffect))]
-public class TextEffectEditor : Editor
+namespace EasyTextEffects.Editor
 {
-    private string manualEffectName_;
-    private string manualTagEffectName_;
-    private bool debugButtonsVisible_;
-
-    public override void OnInspectorGUI()
+    [CustomEditor(typeof(TextEffect))]
+    public class TextEffectEditor : UnityEditor.Editor
     {
-        DrawDefaultInspector();
+        private string manualEffectName_;
+        private string manualTagEffectName_;
+        private bool debugButtonsVisible_;
 
-        var myScript = (TextEffect)target;
-
-        GUILayout.Space(10);
-
-        var buttonStyle = new GUIStyle(GUI.skin.button);
-        buttonStyle.fontSize = 14;
-        buttonStyle.fixedHeight = 30;
-        buttonStyle.fontStyle = FontStyle.Bold;
-
-        var foldoutStyle = new GUIStyle(EditorStyles.foldout)
+        public override void OnInspectorGUI()
         {
-            fontStyle = FontStyle.Bold,
-            fontSize = 14
-        };
+            DrawDefaultInspector();
 
-        if (GUILayout.Button("REFRESH", buttonStyle))
-        {
-            myScript.text.ForceMeshUpdate();
-            myScript.UpdateStyleInfos();
-        }
-
-        GUILayout.BeginVertical("Box");
-
-        DrawFoldoutHeader("Debug Buttons", ref debugButtonsVisible_);
-        if (debugButtonsVisible_)
-        {
-            GUILayout.BeginHorizontal();
-            manualTagEffectName_ = EditorGUILayout.TextField("Manual Tag Effect Name", manualTagEffectName_);
-            if (GUILayout.Button("START"))
-            {
-                myScript.StartManualEffect(manualTagEffectName_);
-            }
-            GUILayout.EndHorizontal();
-            
-            GUILayout.BeginHorizontal();
-            manualEffectName_ = EditorGUILayout.TextField("Manual Global Effect Name", manualEffectName_);
-            if (GUILayout.Button("START"))
-            {
-                myScript.StartManualTagEffect(manualEffectName_);
-            }
-            GUILayout.EndHorizontal();
+            var myScript = (TextEffect)target;
 
             GUILayout.Space(10);
+
+            var buttonStyle = new GUIStyle(GUI.skin.button);
+            buttonStyle.fontSize = 14;
+            buttonStyle.fixedHeight = 30;
+            buttonStyle.fontStyle = FontStyle.Bold;
+
+            if (GUILayout.Button("REFRESH", buttonStyle))
+            {
+                myScript.text.ForceMeshUpdate();
+                myScript.UpdateStyleInfos();
+            }
+
+            GUILayout.BeginVertical("Box");
+
+            DrawFoldoutHeader("Debug Buttons", ref debugButtonsVisible_);
+            if (debugButtonsVisible_)
+            {
+                GUILayout.BeginHorizontal();
+                manualTagEffectName_ = EditorGUILayout.TextField("Manual Tag Effect Name", manualTagEffectName_);
+                if (GUILayout.Button("START"))
+                {
+                    myScript.StartManualEffect(manualTagEffectName_);
+                }
+                GUILayout.EndHorizontal();
             
-            GUILayout.BeginHorizontal();
-            if (GUILayout.Button("START Tag Manual Effects", buttonStyle))
-            {
-                myScript.StartManualTagEffects();
+                GUILayout.BeginHorizontal();
+                manualEffectName_ = EditorGUILayout.TextField("Manual Global Effect Name", manualEffectName_);
+                if (GUILayout.Button("START"))
+                {
+                    myScript.StartManualTagEffect(manualEffectName_);
+                }
+                GUILayout.EndHorizontal();
+
+                GUILayout.Space(10);
+            
+                GUILayout.BeginHorizontal();
+                if (GUILayout.Button("START Tag Manual Effects", buttonStyle))
+                {
+                    myScript.StartManualTagEffects();
+                }
+
+                if (GUILayout.Button("STOP", buttonStyle))
+                {
+                    myScript.StopManualTagEffects();
+                }
+                GUILayout.EndHorizontal();
+
+                GUILayout.BeginHorizontal();
+                if (GUILayout.Button("START Global Manual Effects", buttonStyle))
+                {
+                    myScript.StartManualEffects();
+                }
+
+                if (GUILayout.Button("STOP", buttonStyle))
+                {
+                    myScript.StopManualEffects();
+                }
+                GUILayout.EndHorizontal();
             }
 
-            if (GUILayout.Button("STOP", buttonStyle))
-            {
-                myScript.StopManualTagEffects();
-            }
-            GUILayout.EndHorizontal();
 
-            GUILayout.BeginHorizontal();
-            if (GUILayout.Button("START Global Manual Effects", buttonStyle))
-            {
-                myScript.StartManualEffects();
-            }
-
-            if (GUILayout.Button("STOP", buttonStyle))
-            {
-                myScript.StopManualEffects();
-            }
-            GUILayout.EndHorizontal();
+            GUILayout.EndVertical();
         }
 
-
-        GUILayout.EndVertical();
-    }
-
-    private void DrawFoldoutHeader(string title, ref bool foldoutState)
-    {
-        // Create a horizontal layout for the foldout header
-        EditorGUILayout.BeginHorizontal("box"); // Box for the header background
-        GUILayout.Label(title, EditorStyles.boldLabel);
-
-        // Right-aligned label for "(Click to expand)"
-        GUILayout.FlexibleSpace();
-        GUIStyle clickToExpandStyle = new GUIStyle(EditorStyles.label)
+        private void DrawFoldoutHeader(string title, ref bool foldoutState)
         {
-            fontStyle = FontStyle.Italic,
-            fontSize = 12
-        };
-        GUILayout.Label(foldoutState ? "(Click to collapse)" : "(Click to expand)", clickToExpandStyle);
+            // Create a horizontal layout for the foldout header
+            EditorGUILayout.BeginHorizontal("box"); // Box for the header background
+            GUILayout.Label(title, EditorStyles.boldLabel);
 
-        // Check for mouse click to toggle foldout state
-        Rect headerRect = GUILayoutUtility.GetLastRect();
-        if (Event.current.type == EventType.MouseDown && headerRect.Contains(Event.current.mousePosition))
-        {
-            foldoutState = !foldoutState; // Toggle foldout state
-            Event.current.Use();
+            // Right-aligned label for "(Click to expand)"
+            GUILayout.FlexibleSpace();
+            GUIStyle clickToExpandStyle = new GUIStyle(EditorStyles.label)
+            {
+                fontStyle = FontStyle.Italic,
+                fontSize = 12
+            };
+            GUILayout.Label(foldoutState ? "(Click to collapse)" : "(Click to expand)", clickToExpandStyle);
+
+            // Check for mouse click to toggle foldout state
+            Rect headerRect = GUILayoutUtility.GetLastRect();
+            if (Event.current.type == EventType.MouseDown && headerRect.Contains(Event.current.mousePosition))
+            {
+                foldoutState = !foldoutState; // Toggle foldout state
+                Event.current.Use();
+            }
+
+            EditorGUILayout.EndHorizontal();
         }
-
-        EditorGUILayout.EndHorizontal();
     }
 }
