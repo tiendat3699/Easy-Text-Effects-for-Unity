56df99aedfd0517b2df8c082cfe1fb8e11f91cc8
Qiaozhi Lei <georgeleiqiaozhi@gmail.com>
2024-12-23 18:42:03 -0800

    composite custom editor

---
+++ Editor/CompositeEffectEditor.cs
@@ -0,0 +1,21 @@
+using EasyTextEffects.Effects;
+using UnityEditor;
+using UnityEngine;
+
+namespace EasyTextEffects.Editor
+{
+    [CustomEditor(typeof(Effect_Composite))]
+    public class CompositeEffectEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            var myScript = (Effect_Composite)target;
+            
+            // draw the list of effects 
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("effectName"), true);
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("effects"), true);
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ TODO.md
@@ -0,0 +1,17 @@
+# TODO
+
+## MUST
+- [ ] Demo Scene
+  - [ ] add github link
+- [ ] Documentation
+  - [ ] md files
+  - [ ] inspector
+- [x] Composite
+  - [x] custom editor
+- [ ] Clean Up
+  - [x] Remove persistent
+  - [ ] Organize effects in demo 
+
+## COULD
+
+- [ ] Per vertex effects
